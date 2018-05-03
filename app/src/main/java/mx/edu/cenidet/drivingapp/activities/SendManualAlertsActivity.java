1728413836eac3e2a9494c24acbc635e09d65ffa
racf <racf1991@gmail.com>
2018-05-03 14:13:05 -0500

    Responsibe send alert

--- app/src/main/java/mx/edu/cenidet/drivingapp/activities/SendManualAlertsActivity.java
+++ app/src/main/java/mx/edu/cenidet/drivingapp/activities/SendManualAlertsActivity.java
@@ -82,7 +82,7 @@ public class SendManualAlertsActivity extends AppCompatActivity implements SendD
 
         // Making notification bar transparent
         if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
