5f16cf5c698c42a905ae4f25b4224b6d3b6db27b
haidee09 <haideeram97@gmail.com>
2018-06-19 00:41:57 -0500

    webview security guards fixed, load the page in the same view

--- app/src/main/java/mx/edu/cenidet/app/activities/WebViewSmartSecurity.java
+++ app/src/main/java/mx/edu/cenidet/app/activities/WebViewSmartSecurity.java
@@ -2,7 +2,9 @@ package mx.edu.cenidet.app.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import mx.edu.cenidet.app.R;
 
@@ -13,7 +15,16 @@ public class WebViewSmartSecurity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view_smart_security);
         WebView myWebView = (WebView) findViewById(R.id.webView);
-        myWebView.loadUrl("https://smartsdksecurity.com.mx");
+        loadWebViewLoad(myWebView);
+    }
+
+    private void loadWebViewLoad(WebView webview) {
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webview.getSettings().setSupportMultipleWindows(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.loadUrl("https://smartsdksecurity.com.mx");
     }
 
 }
