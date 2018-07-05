27367d2a6632e49abe8bf2a9ca843c3ffd90d502
danieltoo <torresestradadaniel@gmail.com>
2018-07-04 23:47:23 -0500

    Quité texto Wrongway

--- app/src/main/java/mx/edu/cenidet/app/activities/DrivingView.java
+++ app/src/main/java/mx/edu/cenidet/app/activities/DrivingView.java
@@ -282,11 +282,11 @@ public class DrivingView extends AppCompatActivity implements SensorEventListene
             boolean isWrong =  wrong.getBoolean("isWrongWay"); // red
 
             if (isWrong){
-                textWrongEvent.setText("Wrong Way Detection");
+                //textWrongEvent.setText("Wrong Way Detection");
                 floatingWrong.setBackgroundTintList(getResources().getColorStateList(R.color.driving_red));
                 anim(floatingWrong);
             }else{
-                textWrongEvent.setText("");
+                //textWrongEvent.setText("");
                 floatingWrong.setBackgroundTintList(getResources().getColorStateList(R.color.driving_green));
 
             }
